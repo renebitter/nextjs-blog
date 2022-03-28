c5efd65fd17fc1f7fc42dc02cf3c934810761006
Rene <renebitter@gmail.com>
2022-03-28 11:03:09 -0300

    Add dummy posts & images

--- components/home/featured-posts.js
+++ components/home/featured-posts.js
@@ -1,9 +1,11 @@
+import PostGrid from '../posts/posts-grid';
 import classes from './featured-posts.module.css';
 
-const FeaturedPosts = () => {
+const FeaturedPosts = (props) => {
   return (
     <section className={classes.latest}>
       <h2>FeaturedPosts</h2>
+      <PostGrid posts={props.posts} />
     </section>
   );
 };
