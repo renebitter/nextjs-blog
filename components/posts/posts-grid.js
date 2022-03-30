f0df5ce32cb8616dbfb5804ec5aa44c44a41a9c0
Rene <renebitter@gmail.com>
2022-03-29 23:00:19 -0300

    Add all-posts page

--- components/posts/posts-grid.js
+++ components/posts/posts-grid.js
@@ -3,6 +3,7 @@ import classes from './posts-grid.module.css';
 
 const PostGrid = (props) => {
   const { posts } = props;
+
   return (
     <ul className={classes.grid}>
       {posts.map((post) => (
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,4 +1,37 @@
+import AllPosts from '../../components/posts/all-posts';
+
+const DUMMY_POSTS = [
+  {
+    slug: 'slug-nextjs',
+    title: 'Title Nextjs',
+    image: 'image.jpg',
+    excerpt: 'Excerpt lorem ipsum',
+    date: '2022-01-01',
+  },
+  {
+    slug: 'slug-nextjs-2',
+    title: 'Title Nextjs',
+    image: 'image.jpg',
+    excerpt: 'Excerpt lorem ipsum',
+    date: '2022-01-01',
+  },
+  {
+    slug: 'slug-nextjs-3',
+    title: 'Title Nextjs',
+    image: 'image.jpg',
+    excerpt: 'Excerpt lorem ipsum',
+    date: '2022-01-01',
+  },
+  {
+    slug: 'slug-nextjs-4',
+    title: 'Title Nextjs',
+    image: 'image.jpg',
+    excerpt: 'Excerpt lorem ipsum',
+    date: '2022-01-01',
+  },
+];
+
 const AllPostsPage = () => {
-  return <div>All Posts Page</div>;
+  return <AllPosts posts={DUMMY_POSTS} />;
 };
 export default AllPostsPage;
