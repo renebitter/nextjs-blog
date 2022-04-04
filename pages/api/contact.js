df5deaa8da7425126c4beb27d4a7b0089d771f71
Rene <renebitter@gmail.com>
2022-04-04 12:02:44 -0300

    Add mongodb & connect to DB

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-const handler = (req, res) => {
+import { MongoClient } from 'mongodb';
+
+const handler = async (req, res) => {
   if (req.method === 'POST') {
     const { email, name, message } = req.body;
 
@@ -21,7 +23,28 @@ const handler = (req, res) => {
       message,
     };
 
-    console.log(newMessage);
+    let client;
+
+    try {
+      client = await MongoClient.connect(process.env.MONGODB_URI);
+    } catch (error) {
+      res.status(500).json({ message: 'Could not connect to DB' });
+      console.error(`Error: ${error}`);
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection('messages').insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      res.status(500).json({ message: 'Could not connect to DB' });
+      console.error(`Error: ${error}`);
+      return;
+    }
+
+    client.close();
 
     res
       .status(201)
