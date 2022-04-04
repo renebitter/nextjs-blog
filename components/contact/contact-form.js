c017fb909e4e324db4b24cb2affaa2b3d834538e
Rene <renebitter@gmail.com>
2022-04-04 09:21:59 -0300

    Add contact form

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,6 +1,32 @@
 import classes from './contact-form.module.css';
 
 const ContactForm = () => {
-  return <div className={classes.contact}>Contact Form</div>;
+  return (
+    <section className={classes.contact}>
+      <h1>Contact Form</h1>
+      <form className={classes.form}>
+        <div className={classes.controls}>
+          <div className={classes.control}>
+            <label htmlFor='email'>Your Email</label>
+            <input type='email' id='email' required />
+          </div>
+
+          <div className={classes.control}>
+            <label htmlFor='name'>Your Name</label>
+            <input type='text' id='name' required />
+          </div>
+        </div>
+
+        <div className={classes.control}>
+          <label htmlFor='message'>Your Message</label>
+          <textarea id='name' rows='5' required />
+        </div>
+
+        <div className={classes.actions}>
+          <button>Submit</button>
+        </div>
+      </form>
+    </section>
+  );
 };
 export default ContactForm;
